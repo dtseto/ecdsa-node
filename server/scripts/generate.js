13744f8e36b41af29a6e9a0346ad390ec5bab19c
dtseto <67933197+dtseto@users.noreply.github.com>
2023-01-14 12:53:43 -0800

    Update generate.js

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -1,8 +1,10 @@
 const secp = require("ethereum-cryptography/secp256k1");
 const { toHex } = require("ethereum-cryptography/utils");
+const { keccak256 } = require("ethereum-cryptography/keccak");
 
 const privateKey = secp.utils.randomPrivateKey();
 const publicKey = secp.getPublicKey(privateKey);
+const address = keccak256(publicKey).slice(64-40);
 
 console.log('private key:', toHex(privateKey));
 console.log('address:', publicKey )
